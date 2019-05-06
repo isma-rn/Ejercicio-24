750f8df6b130f03c919fb07a6ca44118f9d1fdc6
Juan Ismael Rosas Nava <JIsmael-RN@outlook.com>
2019-05-05 22:48:06 -0500

    Inicio

---
+++ E24_ImprimirNombre/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E24_ImprimirNombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Juan Ismael Rosas Nava");
+            Console.ReadLine();
+        }
+    }
+}
